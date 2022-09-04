5bac038be3e57b80afe1eb7809b31322b2d6c681
ihsmythe <ihsmythe@shaw.ca>
2022-09-04 15:38:40 -0700

    Removed copyLink
    Turns out copyLink doesn't work. Neither is autoplay encouraged. Moved pieces into latr.js and dynamically added options upon page load.

--- latr/js/latr.js
+++ latr/js/latr.js
@@ -3,36 +3,71 @@ if (top.frames.length != 0) {
     top.location = self.document.location;
 }
 
+class Piece {
+    constructor(filename, title) {
+        this.mp3 = `mp3/${filename}.mp3`;
+        this.ogg = `ogg/${filename}.ogg`;
+        this.title = title;
+    }
+}
+
+const defaultPiece = 2;
+const pieces = [
+    new Piece("Who's Who", "Who's Who (Vol. 1)"),
+    new Piece("Not Much To Ask", "Not Much to Ask (Vol. 1)"),
+    new Piece("Ophelia", "Ophelia (Vol. 1)"),
+    new Piece("Falling Out", "Falling Out (Vol. 1)"),
+    new Piece("Ariadne's Thread", "Ariadne's Thread (Vol. 1)"),
+    new Piece("Ariel's Song", "Ariel's Song (Vol. 1)"),
+    new Piece("Culture", "Culture (Vol. 1)"),
+    new Piece("Lost Lyric", "Lost Lyric (Vol. 1)"),
+    new Piece("Education", "Education (Vol. 1)"),
+    new Piece("Inside Story", "Inside Story (Vol. 1)"),
+    new Piece("Life Happened", "Life Happened (Vol. 1)"),
+    new Piece("The Poet At Sixty", "The Poet At Sixty (Vol. 1)"),
+    new Piece("The Future", "The Future (Vol. 1)"),
+    new Piece("If It Were Not So", "If It Were Not So (Vol. 1)"),
+    new Piece("Ephemera", "Ephemera (Vol. 1)"),
+    new Piece("More Monsters", "More Monsters (Vol. 1)"),
+    new Piece("Summer Day In The Tuileries", "Summer Day In The Tuileries (Vol. 1)"),
+    new Piece("Exile", "Exile (Vol. 1)"),
+    new Piece("Valediction", "Valediction (Vol. 1)"),
+    new Piece("Saved - Healing", "Saved - Healing (Vol. 1)"),
+    new Piece("Church Service", "Church Service (Vol. 2)"),
+    new Piece("Advice to Other Writers", "Advice to Other Writers (Vol. 2)")
+];
+
 // Stop, replace audio source, and start the audio player
-function loadAudioSource(title) {
+function loadAudioSource(piece) {
     mp3 = document.getElementById("audioMP3");
     ogg = document.getElementById("audioOGG");
     // Load new sources
-    mp3.src = "mp3/" + title + ".mp3";
-    ogg.src = "ogg/" + title + ".ogg";
+    mp3.src = piece.mp3;
+    ogg.src = piece.ogg;
     player = document.getElementById("player");
     player.pause(); // Stop playing
     player.currentTime = 0; // Set to start
     player.load(); // Load new sources
-    player.play(); // Start playing
-}
-
-// Update the audio source if a different title is selected
-function updateAudioSource() {
-    loadAudioSource(document.getElementById("latr").value);
+    // Do not autoplay: player.play(); // Start playing
 }
 
 // Update the audio source from a query parameter, or set a default value
 function selectAudioSource() {
-    // Set default title if no title provided in 'title' query parameter
-    const selectedTitle = new URLSearchParams(window.location.search).get('title') || 'Ophelia';
-    document.getElementById("latr").value = selectedTitle;
-    loadAudioSource(selectedTitle);
-}
+    // If latr is not yet populated, populate it with pieces
+    const latrSelect = document.getElementById("latr");
+    for (let i = 0; i < pieces.length; i++) {
+        // Can't do pieces.map((piece) => { because we need the index item as the value in the option element
+        const opt = document.createElement('option');
+        opt.appendChild(document.createTextNode(pieces[i].title));
+        opt.value = i;
+        latrSelect.appendChild(opt);
+    };
 
-// Write a link to load this piece to the user's clipboard
-function copyLink() {
-    // Get current location minus the search params
-    const link = `${window.location.origin}${window.location.pathname}?title=${document.getElementById("latr").value}`;
-    navigator.clipboard.writeText(link);
+    const selectedLatr = new URLSearchParams(window.location.search).get('latr');
+    let selectedIndex = parseInt(selectedLatr);
+    if (isNaN(selectedIndex) || selectedIndex < 0 || selectedIndex >= pieces.length) {
+        selectedIndex = defaultPiece;
+    }
+    latrSelect.value = selectedIndex;
+    loadAudioSource(pieces[selectedIndex]);
 }
